f7477633e2c740917f628fa3b653f7e186034e8e
[dyad] <77tech.progamer@gmail.com>
2025-11-29 17:45:25 +0530

    [dyad] wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer'; // Import the Footer component
 
-const Layout = () => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-grow">
-        <Outlet />
+        {children} {/* Render children here */}
       </main>
       <Footer /> {/* Include the Footer component here */}
     </div>
