229717236bf4a50f7bd1b22d73985baea0a5f110
[dyad] <77tech.progamer@gmail.com>
2025-11-30 13:29:45 +0530

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -78,7 +78,7 @@ const Index = () => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Hero Section */}
-      <section className="relative w-full min-h-[40vh] bg-cover bg-center flex items-end justify-center"> {/* Changed items-start to items-end */}
+      <section className="relative w-full min-h-[40vh] bg-cover bg-center flex items-start justify-center"> {/* Changed items-end to items-start */}
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="relative z-10 text-white w-full h-full flex flex-col justify-between items-center text-center p-4">
           <DateTimeDisplay className="absolute top-4 right-4" /> {/* Adjusted top-0 right-0 to top-4 right-4 */}
