2e09a722c6a137839b49cc8369ac5b3493179077
77techprogamer-sketch <77tech.progamer@gmail.com>
2026-01-20 13:23:34 +0530

    feat: Add brand favicon SVG.

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,7 +62,9 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - robots.txt (robots file)
+         * - sitemap.xml (sitemap file)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
     ],
 };
