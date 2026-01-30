e4406f2f84e1a942b5bebbf62cc1bd8254fbc3ab
77techprogamer-sketch <77tech.progamer@gmail.com>
2026-01-30 10:42:55 +0530

    Update Microsoft Clarity tracking ID

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -13,7 +13,7 @@ const Analytics = () => {
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "v187agkimc");
+          })(window, document, "clarity", "script", "v9dto5m10l");
         `,
             }}
         />
