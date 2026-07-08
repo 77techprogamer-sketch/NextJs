c93a0fd736a1f463c76f4a717247b48e3da3c1e3
Kali User <kali@example.com>
2026-07-08 12:27:10 +0530

    feat: Add hourly ping script

---
+++ scripts/hourly-ping.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -s -X POST "https://api.indexnow.org/indexnow" -H "Content-Type: application/json" -d "{\"host\":\"insurancesupport.online\",\"key\":\"71a80a3568ae5d1d945fda3ef57fe18e\",\"urlList\":[\"https://insurancesupport.online\"]}"
